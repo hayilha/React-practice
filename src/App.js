44aed9a1505eda6d9c4ff5d1bd3db9dde2d56e89
Ahliyah <ahliyahazeez@gmail.com>
2024-11-04 09:03:01 +0000

    Login page

--- src/App.js
+++ src/App.js
@@ -1,41 +1,14 @@
 import React from 'react';
-import Userprofile from './components/Userprofile'
-import Project from './components/Project';
-import Header from './components/Header';
+import Login from './components/Login'; 
 
-
-/*import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      <Login/>
     </div>
   );
-}
+};
 
-export default App;*/
-const App =() =>{
-  
-  return <div> 
-    <Header/>
-    <h1>Root Component</h1>
-<Userprofile age={20} Username="Amala"/>
-<Project/>
-  </div>
-}
 export default App;
+
